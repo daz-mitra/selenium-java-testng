3c858b809fccb1462638b67c729cca07a8669c5f
Dasun Eranthika <eshantha.peiris@travisperkins.co.uk>
2022-07-24 15:40:28 +0530

    add headless option

--- src/main/java/utils/Drivers.java
+++ src/main/java/utils/Drivers.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
 import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
@@ -41,6 +42,12 @@ public class Drivers {
                     driver= new EdgeDriver();
                     driver.manage().window().maximize();
                     break;
+                case "Headless":
+                    ChromeDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless");
+                    driver = new ChromeDriver(chromeOptions);
+                    driver.manage().window().maximize();
                 default:
                     ChromeDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
